b50cff6030d562e8200e3aa6ac58edf4e7490da6
g24force <102439759+g24force@users.noreply.github.com>
2024-02-02 18:03:31 +0000

    Add current score display

--- main.js
+++ main.js
@@ -9,23 +9,37 @@ function getComputerChoice () {
     };
 };
 
-function roundResult (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-    computerSelection = computerSelection.toLowerCase();
-    computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-    if (playerSelection === computerSelection) {
-        return "It's a draw!";
-    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else {
-    return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
-    };
-}
+let playerScore = 0;
+let computerScore = 0;
+
+const currentScore = document.querySelector('#current-score');
+
+// for (i = 1; i <= playerScore || i <= computerScore; i++) {
+    function roundResult (playerSelection, computerSelection) {
+        console.log(playerSelection)
+        console.log(computerSelection)
+        if (playerSelection === computerSelection) {
+            return "It's a draw!";
+        } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+            playerScore = playerScore + 1;
+            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+        } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
+            playerScore = playerScore + 1;
+            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+        } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+            playerScore = playerScore + 1;
+            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+        } else {
+            computerScore = computerScore + 1;
+            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+        };
+    }  
+// }
+
 const div = document.querySelector('div')
 
 const rock = document.querySelector('#rock');
@@ -42,21 +56,3 @@ const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
     div.textContent = roundResult ('Scissors', getComputerChoice());
 })
-
-let playerScore = 0;
-let computerScore = 0;
-
-const currentScore = document.querySelector('#current-score');
-
-for (i = 1; i <= playerScore || i <= computerScore; i++) {
-    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        let playerScore = playerScore + 1;
-        currentScore.textContent = "Player " + playerScore + " : " + computerScore;
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else {
-    return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
-    };
-}
