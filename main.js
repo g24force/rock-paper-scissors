ea53b40f55725d32ce184394c5e238c3d61692d0
g24force <102439759+g24force@users.noreply.github.com>
2024-02-04 23:06:25 +0000

    Improve page's design

--- main.js
+++ main.js
@@ -12,35 +12,36 @@ function getComputerChoice () {
 let playerScore = 0;
 let computerScore = 0;
 
-const currentScore = document.querySelector('#current-score');
+const playerScoreDisplay = document.querySelector('#player-score');
+const computerScoreDisplay= document.querySelector('#computer-score');
 
 // for (i = 1; i <= playerScore || i <= computerScore; i++) {
     function roundResult (playerSelection, computerSelection) {
-        console.log(playerSelection)
-        console.log(computerSelection)
+        console.log(playerSelection);
+        console.log(computerSelection);
         if (playerSelection === computerSelection) {
             return "It's a draw!";
         } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
             playerScore = playerScore + 1;
-            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            playerScoreDisplay.textContent = playerScore;
             return 'You win! ' + playerSelection + ' beats ' + computerSelection;
         } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
             playerScore = playerScore + 1;
-            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            playerScoreDisplay.textContent = playerScore;
             return 'You win! ' + playerSelection + ' beats ' + computerSelection;
         } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
             playerScore = playerScore + 1;
-            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            playerScoreDisplay.textContent = playerScore;
             return 'You win! ' + playerSelection + ' beats ' + computerSelection;
         } else {
             computerScore = computerScore + 1;
-            currentScore.textContent = "Player " + playerScore + " : " + computerScore;
+            computerScoreDisplay.textContent = computerScore;
             return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
         };
     }  
 // }
 
-const resultDisplay = document.querySelector('#result-display')
+const resultDisplay = document.querySelector('#round-winner')
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
