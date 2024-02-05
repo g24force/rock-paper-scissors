fe37a37b0a791613313af127e726009a8307c63d
g24force <102439759+g24force@users.noreply.github.com>
2024-02-05 13:54:50 +0000

    End game after 5 wins from player/computer

--- main.js
+++ main.js
@@ -9,53 +9,10 @@ function getComputerChoice () {
     };
 };
 
-/* let playerScore = 0;
-let computerScore = 0; */
-
 const playerScoreDisplay = document.querySelector('#player-score');
 const computerScoreDisplay= document.querySelector('#computer-score');
-
 const resultDisplay = document.querySelector('#round-winner')
-
-/* const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => {
-    resultDisplay.textContent = roundResult ('Rock', getComputerChoice());
-})
-
-const paper = document.querySelector('#paper');
-paper.addEventListener('click', () => {
-    resultDisplay.textContent = roundResult ('Paper', getComputerChoice());
-})
-
-const scissors = document.querySelector('#scissors');
-scissors.addEventListener('click', () => {
-    resultDisplay.textContent = roundResult ('Scissors', getComputerChoice());
-}) */
-
-
-/* function roundResult (playerSelection, computerSelection) {
-    console.log(playerSelection);
-    console.log(computerSelection);
-    if (playerSelection === computerSelection) {
-        return "It's a draw!";
-    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        playerScore = playerScore + 1;
-        playerScoreDisplay.textContent = playerScore;
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
-        playerScore = playerScore + 1;
-        playerScoreDisplay.textContent = playerScore;
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        playerScore = playerScore + 1;
-        playerScoreDisplay.textContent = playerScore;
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-    } else {
-        computerScore = computerScore + 1;
-        computerScoreDisplay.textContent = computerScore;
-        return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
-    };
-} */
+const display = document.querySelector('#display')
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
@@ -74,42 +31,52 @@ scissors.addEventListener('click', () => {
 
 const gameWinner = document.querySelector('#game-winner')
 
-function playRound (playerSelection) {
-    let playerScore = 0;
-    let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
-    if (playerScore < 5 && computerScore < 5) {
-        function roundResult (playerSelection, computerSelection) {
-            computerSelection = getComputerChoice();
-            console.log(playerSelection);
-            console.log(computerSelection);
-            if (playerSelection === computerSelection) {
-                return "It's a draw!";
-            } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-                playerScore = playerScore + 1;
-                playerScoreDisplay.textContent = playerScore;
-                return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-            } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
-                playerScore = playerScore + 1;
-                playerScoreDisplay.textContent = playerScore;
-                return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-            } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-                playerScore = playerScore + 1;
-                playerScoreDisplay.textContent = playerScore;
-                return 'You win! ' + playerSelection + ' beats ' + computerSelection;
-            } else {
-                computerScore = computerScore + 1;
-                computerScoreDisplay.textContent = computerScore;
-                return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
-            };
-        }
+function roundResult (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
+        return 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    } else {
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
+        return 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+    };
+}
+
+const restartButton = document.createElement('button');
+restartButton.textContent = "Restart game";
+restartButton.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreDisplay.textContent = 0;
+    computerScoreDisplay.textContent = 0;
+    resultDisplay.textContent = '';
+    gameWinner.textContent = '';
+    display.removeChild(restartButton);
+})
+
+function playRound (playerChoice) {
+    console.log(playerScore, computerScore)
+    if (playerScore <= 4 && computerScore <= 4) {
+        return roundResult(playerChoice, getComputerChoice());
     } else if (playerScore === 5) {
-        // Player wins the game (pôr isto em overlay, se conseguir)
-        gameWinner.textContent = 'Player wins the game';
-        // adicionar um botão de restart que limpa tudo
+        gameWinner.textContent = 'Player wins the game!';
+        display.appendChild(restartButton);
     } else if (computerScore === 5) {
-        // Computer wins the game (pôr isto em overlay, se conseguir)
-        gameWinner.textContent = 'Computer wins the game';
-        // adicionar um botão de restart que limpa tudo
+        gameWinner.textContent = 'Computer wins the game!';
+        display.appendChild(restartButton);
     }
 }
